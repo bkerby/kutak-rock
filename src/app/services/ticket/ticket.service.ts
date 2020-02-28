a9847cf8d32fb221a5f3271aecd161cd4d48c43f
Benjamin Kerby <benjaminkerby@ext-000B14-0862.omhq.uprr.com>
2020-02-28 14:24:39 -0600

    Fixed CORS issue

--- src/app/services/ticket/ticket.service.ts
+++ src/app/services/ticket/ticket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UtilitiesService } from '../utilities/utilities.service';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,21 @@ export class TicketService {
   constructor(private http: HttpClient, private utils: UtilitiesService) { }
 
   get(subject: string, description: string, proirity: number, computerId: number) {
+    return from(
+      fetch(
+        `http://kutakrockwcf.azurewebsites.net/Database_Service.svc/CreateTicket/${subject}/${description}/${proirity}/${computerId}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'GET',
+          mode: 'no-cors'
+        }
+      )
+    )
+  }
+
+  get2(subject: string, description: string, proirity: number, computerId: number) {
     return this.http.get(`Database_Service.svc/CreateTicket/${subject}/${description}/${proirity}/${computerId}`);
   }
 }
