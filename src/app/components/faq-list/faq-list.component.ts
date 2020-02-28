1b1df8d6a9c2ce04ddc01f5dd03e6a4c20060b1c
Benjamin Kerby <benjaminkerby@ext-000B14-0862.omhq.uprr.com>
2020-02-28 15:08:15 -0600

    update clean

--- src/app/components/faq-list/faq-list.component.ts
+++ src/app/components/faq-list/faq-list.component.ts
@@ -8,7 +8,7 @@ import { UtilitiesService } from 'src/app/services/utilities/utilities.service';
   styleUrls: ['./faq-list.component.scss']
 })
 export class FaqListComponent implements OnInit {
-  @ViewChild('searchbar', {static: false}) searchbar: ElementRef;
+  @ViewChild('searchbar') searchbar: ElementRef;
   list = listData.sort((a, b) => (a.asked < b.asked) ? 1 : -1);
   searchText = '';
 
